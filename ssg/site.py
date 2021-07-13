921fcf9954dd81193da3e223ac51df9752946d88
jcolli <Zuul06@gmail.com>
2021-07-13 09:15:50 -0700

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,13 +1,16 @@
 from genericpath import exists, isdir
 from os import mkdir
 from pathlib import Path
-import os
 import shutil
+from ssg.parsers import Parser
 
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = None
+        # ToDo: In constructor body set new instance variable parsers to expression parsers or []
+        
         
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -20,3 +23,18 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                Site.run_parser(path)
+
+    def load_parser(extension):
+        for parser in self.parsers:
+            if Parser.valid_extension(extension):
+                return parser
+
+    def run_parser(path):
+        parser = Site.load_parser(path.suffix)
+
+        if parser is not None:
+            Parser.parse(path, Parser.source, Parser.dest)
+        else:
+            print("Not Implemented")
