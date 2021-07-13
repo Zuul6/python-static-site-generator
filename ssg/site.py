7b2ea45011f4228b0b17a79d073aaf015b26756f
jcolli <Zuul06@gmail.com>
2021-07-13 09:34:17 -0700

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -5,7 +5,7 @@ import shutil
 from ssg.parsers import Parser
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers):
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = None
@@ -24,17 +24,17 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                Site.run_parser(path)
+                self.run_parser(path)
 
-    def load_parser(extension):
+    def load_parser(self, extension):
         for parser in self.parsers:
             if Parser.valid_extension(extension):
                 return parser
 
-    def run_parser(path):
+    def run_parser(self, path):
         parser = Site.load_parser(path.suffix)
 
         if parser is not None:
-            Parser.parse(path, Parser.source, Parser.dest)
+            Parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
